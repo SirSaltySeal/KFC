f157fec9eb1481a256842bc94b5e0f3a547cd3b6
SirSaltySeal <S10197904@connect.np.edu.sg>
2023-07-24 12:25:42 +0800

    Main Enemy AI now follows player

--- KFC/Assets/Scripts/PlayerLook.cs
+++ KFC/Assets/Scripts/PlayerLook.cs
@@ -1,18 +1,50 @@
+/*
+ * Author: Tanucan Cliford Baguio
+ * Date: 24/07/2023
+ * Description: Player Movement
+ */
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerLook : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float sensX;
+    [SerializeField] private float sensY;
+    
+    Camera cam;
+
+    float mouseX;
+    float mouseY;
+
+    float multiplier = 0.01f;
+
+    float xRotation;
+    float yRotation;
+
+    private void Start()
     {
-        
+        cam = GetComponentInChildren<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        myInput();
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        transform.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+
+    void myInput()
+    {
+        mouseX = Input.GetAxisRaw("Mouse X");
+        mouseY = Input.GetAxisRaw("Mouse Y");
+
+        yRotation += mouseX * sensX * multiplier ; //look horizontally
+        xRotation -= mouseY * sensY * multiplier ; //look vertically & subtract to prevent inversion when looking
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); //Player does not look too far back vertically
     }
 }
